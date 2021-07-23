fe58f240c2cbacb074188c7ecbb2f31cb437a6a3
Noah Polackal <71651076+NoahPolackal@users.noreply.github.com>
2021-07-23 12:31:03 +0530

    Add files via upload

---
+++ No.GGame.py
@@ -0,0 +1,17 @@
+print('Number Guessing Game')
+print('Enter a number between 1-10')
+
+
+firstGuess=int(input('Enter your first guess:'))
+
+
+if firstGuess==7:
+    print("Superb! You guessed the Number !!")
+elif firstGuess<3:
+    print("Too low, try a higher number...")
+elif firstGuess==5:
+    print("Close... Keep trying")
+elif firstGuess==6:
+    print("Close... Keep trying")
+elif firstGuess>11:
+    print("Too far, try a lower number")
