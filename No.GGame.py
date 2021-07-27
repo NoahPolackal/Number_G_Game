6169cd040d03e85d9238a1076227dc82963bdd7e
Noah Polackal <71651076+NoahPolackal@users.noreply.github.com>
2021-07-27 16:09:36 +0530

    Update No.GGame.py

--- No.GGame.py
+++ No.GGame.py
@@ -7,21 +7,9 @@ firstGuess=int(input('Enter your first guess:'))
 
 if firstGuess==7:
     print("Superb! You guessed the Number !!")
-elif firstGuess<3:
+elif firstGuess<5:
     print("Too low, try a higher number...")
-elif firstGuess<2:
-    print("Too low, try a higher number...")
-elif firstGuess<4:
-    print("Too low, try a higher number...")
-elif firstGuess==5:
+elif firstGuess===6:
     print("Close... Keep trying")
-elif firstGuess==8:
-    print("Too far, try a lower number")
-elif firstGuess==9:
-    print("Too far, try a lower number")
-elif firstGuess==6:
-    print("Close... Keep trying")
-elif firstGuess==1:
-    print("Too low, try a higher number...")
-elif firstGuess>11:
+elif firstGuess>10:
     print("Too far, try a lower number")
